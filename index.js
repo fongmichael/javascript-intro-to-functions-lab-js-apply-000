d25248fb83b7a951c62cfadf3774d61d0a237b8c
Michael Fong <michaelfong88@gmail.com>
2018-06-05 03:49:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ function whisper(string) {
   return string.toLowerCase()
 }
 function logShout(string) {
-  console.log(HELLO)
+  console.log("HELLO")
 }
 function logWhisper(string) {
-  console.log(hello)
+  console.log("hello")
 }
