ea1057d446ed31534441fa7b4fcadaaa4310e5c7
Michael Fong <michaelfong88@gmail.com>
2018-06-05 03:35:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function logWhisper(string) {
   console.log.toLowerCase ()
 }
 function sayHiToGrandma(string) {
-  return I can\t hear you!.if string is lowercase()
+  return I can\'t hear you!.if string is lowercase()
   return YES INDEED!.if string is uppercase()
   return I love you, too.if string is I love you, Grandma.()
 }
