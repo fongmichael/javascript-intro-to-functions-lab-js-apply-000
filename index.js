7ed23aac17bbad844d1948fcf928bfe0a828dbf8
Michael Fong <michaelfong88@gmail.com>
2018-06-05 03:51:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,3 +10,7 @@ function logShout(string) {
 function logWhisper(string) {
   console.log("hello")
 }
+function sayHiToGrandma(string) {
+  return string "I can\'t hear you!" if string is lowercase
+}
+ 
