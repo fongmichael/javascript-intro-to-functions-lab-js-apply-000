ca382847e0b4f7639ef4c197ad5267dea6cb6138
Michael Fong <michaelfong88@gmail.com>
2018-06-06 01:51:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,24 +5,16 @@ function whisper(string) {
   return string.toLowerCase()
 }
 function logShout(string) {
-  console.log(string.toUppercase());
+  console.log('HELLO');
 }
 function logWhisper(string) {
   console.log('hello')
 }
 function sayHiToGrandma(string) {
-  if (string === string.lowercase()) {
-    return "I can't hear you!"
-  }
+  if (string === string.toLowerCase())
+    return "I can't hear you!";
+else if (string === string.toUpperCase())
+    return "YES INDEED!";
+else if (string === "I love you, Grandma.")
+    return "I love you, too.";
 }
-function sayHiToGrandma(string) {
-  if (string===uppercase) {
-    return "YES INDEED!"
-  }
-}
-function sayHiToGrandma(string) {
-  if ((string)==="I love you,Grandma") {
-    return "I love you, too."
-  }
-}
-learn
